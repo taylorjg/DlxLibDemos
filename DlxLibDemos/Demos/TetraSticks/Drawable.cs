a3665b69b8ac323588d3a30ae87a5f5aed8fa2da
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-12-05 23:43:25 +0000

    Inset TetraSticks line endings a bit

--- DlxLibDemos/Demos/TetraSticks/Drawable.cs
+++ DlxLibDemos/Demos/TetraSticks/Drawable.cs
@@ -103,22 +103,40 @@ public class TetraSticksDrawable : IDrawable
     var location = internalRow.Location;
     var variation = internalRow.Variation;
 
+    var lineEndings = FindLineEndings(internalRow);
+
+    var isLineEnding = (Coords coords) => lineEndings.Exists(c => c == coords);
+
     foreach (var horizontal in variation.Horizontals)
     {
+      var startCoords = horizontal;
+      var endCoords = new Coords(startCoords.Row, startCoords.Col + 1);
+
       var w = _squareWidth;
       var h = _squareHeight * 0.1f;
       var x = CalculateX(location.Col + horizontal.Col);
       var y = CalculateY(location.Row + horizontal.Row) - h / 2;
+
+      if (isLineEnding(startCoords) || isLineEnding(endCoords)) w -= _squareWidth * 0.1f;
+      if (isLineEnding(startCoords)) x += _squareWidth * 0.1f;
+
       canvas.FillColor = colour;
       canvas.FillRectangle(x, y, w, h);
     }
 
     foreach (var vertical in variation.Verticals)
     {
+      var startCoords = vertical;
+      var endCoords = new Coords(startCoords.Row + 1, startCoords.Col);
+
       var w = _squareWidth * 0.1f;
       var h = _squareHeight;
       var x = CalculateX(location.Col + vertical.Col) - w / 2;
       var y = CalculateY(location.Row + vertical.Row);
+
+      if (isLineEnding(startCoords) || isLineEnding(endCoords)) h -= _squareHeight * 0.1f;
+      if (isLineEnding(startCoords)) y += _squareHeight * 0.1f;
+
       canvas.FillColor = colour;
       canvas.FillRectangle(x, y, w, h);
     }
@@ -131,6 +149,35 @@ public class TetraSticksDrawable : IDrawable
     //   canvas.FillColor = Colors.Purple;
     //   canvas.FillCircle(cx, cy, r);
     // }
+  }
+
+  private List<Coords> FindLineEndings(TetraSticksInternalRow internalRow)
+  {
+    var allCoords = new List<Coords>();
+
+    foreach (var horizontal in internalRow.Variation.Horizontals)
+    {
+      allCoords.Add(horizontal);
+      allCoords.Add(new Coords(horizontal.Row, horizontal.Col + 1));
+    }
+
+    foreach (var vertical in internalRow.Variation.Verticals)
+    {
+      allCoords.Add(vertical);
+      allCoords.Add(new Coords(vertical.Row + 1, vertical.Col));
+    }
+
+    var lineEndings = new List<Coords>();
+
+    foreach (var coords in allCoords)
+    {
+      if (allCoords.Count(c => c == coords) == 1)
+      {
+        lineEndings.Add(coords);
+      }
+    }
+
+    return lineEndings;
   }
 
   private float CalculateX(int col) => col * _squareWidth + _gridLineHalfThickness;
