c42750d34e8ff0f55529f0e685867d913c4a77f6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-01-07 10:27:31 +0000

    Create separate scripts to target specific devices in iOS Simulator

--- run-ipad.sh
+++ run-ipad.sh
@@ -1,4 +1,5 @@
 dotnet build DlxLibDemos \
   --configuration Release \
   --framework net6.0-ios \
+  --property:_DeviceName=:v2:udid=9C16B158-428B-45EB-BE70-CB45E73AB663 \
   --target:run
---
+++ run-iphone.sh
@@ -0,0 +1,5 @@
+dotnet build DlxLibDemos \
+  --configuration Release \
+  --framework net6.0-ios \
+  --property:_DeviceName=:v2:udid=72AF9A61-ADCF-4F14-ADA7-CE89276B012E \
+  --target:run
