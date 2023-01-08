c2792d6c6d98644b93796810ec555e431b35eb13
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-01-08 11:29:46 +0000

    Add another Demo Page unit test

--- DlxLibDemos.Tests/DemoPageViewModelTests.cs
+++ DlxLibDemos.Tests/DemoPageViewModelTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using DlxLibDemos.Demos.NQueens;
 
 namespace DlxLibDemos.Tests;
 
@@ -7,10 +9,40 @@ public class DemoPageViewModelTests
   [Fact]
   public void CanConstructViewModel()
   {
-    var mockLogger = new NullLogger<DemoPageBaseViewModel>();
-    var mockSolver = new MockSolver();
+    var tuple = ConstructViewModel();
+    var viewModel = tuple.ViewModel;
+    Assert.NotNull(viewModel);
+  }
+
+  [Fact]
+  public void CanSolve()
+  {
+    var tuple = ConstructViewModel();
+    var viewModel = tuple.ViewModel;
+    var mockSolver = tuple.MockSolver;
+    viewModel.SolveCommand.Execute(null);
+    var options = new SolverOptions(false, 10);
+    mockSolver.Verify(m => m.Solve(
+      options,
+      It.IsAny<Action<BaseMessage>>(),
+      It.IsAny<CancellationToken>(),
+      It.IsAny<IDemo>(),
+      It.IsAny<object>()));
+  }
+
+  private (NQueensDemoPageViewModel ViewModel, Mock<ISolver> MockSolver) ConstructViewModel()
+  {
+    var mockLoggerDemo = new NullLogger<NQueensDemo>();
+    var demo = new NQueensDemo(mockLoggerDemo);
+
+    var mockLoggerBase = new NullLogger<DemoPageBaseViewModel>();
+    var mockSolver = new Mock<ISolver>();
     var mockDispatcherProvider = new MockDispatcherProvider();
-    var dependencies = new DemoPageBaseViewModel.Dependencies(mockLogger, mockSolver, mockDispatcherProvider);
-    var viewModel = new DemoPageBaseViewModel(dependencies);
+    var dependencies = new DemoPageBaseViewModel.Dependencies(mockLoggerBase, mockSolver.Object, mockDispatcherProvider);
+
+    var mockLoggerDerived = new NullLogger<NQueensDemoPageViewModel>();
+    var viewModel = new NQueensDemoPageViewModel(mockLoggerDerived, demo, dependencies);
+
+    return (viewModel, mockSolver);
   }
 }
