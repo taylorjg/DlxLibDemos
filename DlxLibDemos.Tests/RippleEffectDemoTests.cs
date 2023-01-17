018f3999b785997b5933422725c8e9c062d19f57
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-01-17 23:31:10 +0000

    Add unit tests re RippleEffect solving

--- DlxLibDemos.Tests/RippleEffectDemoTests.cs
+++ DlxLibDemos.Tests/RippleEffectDemoTests.cs
@@ -22,26 +22,60 @@ public class RippleEffectDemoTests
   {
     var size = puzzle.Size;
     Assert.Equal(size * size, internalRows.Length);
-    CheckValueAdjacenies(puzzle, internalRows);
+    CheckAdjacenyConstraints(puzzle, internalRows);
     CheckRoomValues(puzzle, internalRows);
   }
 
-  private static void CheckValueAdjacenies(Puzzle puzzle, RippleEffectInternalRow[] internalRows)
+  private static void CheckAdjacenyConstraints(Puzzle puzzle, RippleEffectInternalRow[] internalRows)
   {
-    var size = puzzle.Size;
-
-    foreach (var row in Enumerable.Range(0, size))
+    void CheckAdjacencyConstraint(Coords cell, int value, Func<Coords, Coords> advance)
     {
-      foreach (var col in Enumerable.Range(0, size))
+      var currentCell = cell;
+
+      foreach (var _ in Enumerable.Range(0, value))
       {
+        currentCell = advance(currentCell);
+        var internalRow = internalRows.FirstOrDefault(ir => ir.Cell == currentCell);
+        if (internalRow != null)
+        {
+          var message = $"cell: {cell}; value: {value}; currentCell: {currentCell}";
+          Assert.False(internalRow.Value == value, message);
+        }
       }
+    }
+
+    foreach (var internalRow in internalRows)
+    {
+      CheckAdjacencyConstraint(internalRow.Cell, internalRow.Value, coords => coords.Up());
+      CheckAdjacencyConstraint(internalRow.Cell, internalRow.Value, coords => coords.Down());
+      CheckAdjacencyConstraint(internalRow.Cell, internalRow.Value, coords => coords.Left());
+      CheckAdjacencyConstraint(internalRow.Cell, internalRow.Value, coords => coords.Right());
     }
   }
 
   private static void CheckRoomValues(Puzzle puzzle, RippleEffectInternalRow[] internalRows)
   {
+    int FindValue(Coords cell)
+    {
+      foreach (var internalRow in internalRows)
+      {
+        if (internalRow.Cell == cell) return internalRow.Value;
+      }
+
+      return 0;
+    }
+
     foreach (var room in puzzle.Rooms)
     {
+      var roomSize = room.Cells.Length;
+      var values = new List<int>();
+      foreach (var cell in room.Cells)
+      {
+        var value = FindValue(cell);
+        Assert.InRange(value, 1, roomSize);
+        values.Add(value);
+      }
+      Assert.Equal(roomSize, values.Distinct().Count());
     }
   }
 }
